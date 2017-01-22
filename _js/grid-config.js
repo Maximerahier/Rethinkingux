c5d4edef7032382c0cf11ee1de108e5095568875
RahierMaxime <m_rahier@oulook.com>
2017-01-22 10:35:10 +0100

    fin

---
+++ _js/grid-config.js
@@ -0,0 +1,14 @@
+
+
+  var myGrid = {
+    gridWidthUnit: "px",
+    gridWidth: 1260,
+    gridGutterWidthUnit: "px",
+    gridColumnNumber: 10,
+    gridGutterWidth: 40,
+    gridRowUnit: "px",
+    gridRowHeight: 40,
+    activateGrid: true
+  };
+
+  setCSGrid(myGrid);
